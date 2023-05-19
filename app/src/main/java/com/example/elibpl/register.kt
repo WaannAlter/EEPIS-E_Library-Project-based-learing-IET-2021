387877f74e723b33d70646fb39c3d2b4dc9b347f
WaannAlter <134024741+WaannAlter@users.noreply.github.com>
2023-05-19 09:43:59 +0700

    initial commit

---
+++ app/src/main/java/com/example/elibpl/register.kt
@@ -0,0 +1,11 @@
+package com.example.elibpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
